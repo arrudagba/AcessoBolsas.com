e5ced8c4a4b68c1abae3619873f8f95a3cb7c5c5
arrudagba <garbrielarrudahash@gmail.com>
2024-04-27 18:45:23 -0300

    admin criado

--- AcessoBolsas/institution/models.py
+++ AcessoBolsas/institution/models.py
@@ -1,7 +1,7 @@
 from django.db import models
 from django.contrib.auth.models import User
 
-class PerfilInstituicao(models.Model):
+class Institution(models.Model):
     usuario = models.OneToOneField(User, on_delete=models.CASCADE)
     nome = models.CharField(max_length=100)
     cnpj = models.CharField(max_length=14)
--- AcessoBolsas/institution/views.py
+++ AcessoBolsas/institution/views.py
@@ -1,15 +1,15 @@
 from django.shortcuts import render, redirect
 from django.contrib.auth.decorators import login_required
-from .models import PerfilInstituicao
+from institution.models import Institution
 
 @login_required
 def perfil_instituicao(request):
-    perfil = PerfilInstituicao.objects.get(usuario=request.user)
+    perfil = Institution.objects.get(usuario=request.user)
     return render(request, 'perfil_instituicao.html', {'perfil': perfil})
 
 @login_required
 def editar_perfil(request):
-    perfil = PerfilInstituicao.objects.get(usuario=request.user)
+    perfil = Institution.objects.get(usuario=request.user)
 
     if request.method == 'POST':
         perfil.nome = request.POST['nome']
