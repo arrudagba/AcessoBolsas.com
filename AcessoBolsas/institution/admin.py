835be7c673926b01c8aa0b2ab64ee53ba80e3ec7
arrudagba <garbrielarrudahash@gmail.com>
2024-04-29 13:39:21 -0300

    admin adicionado institutions

--- AcessoBolsas/institution/admin.py
+++ AcessoBolsas/institution/admin.py
@@ -1,4 +1,6 @@
 from django.contrib import admin
 from institution.models import Institution
-# Register your models here.
+
+
+
 admin.site.register(Institution)
--- AcessoBolsas/institution/models.py
+++ AcessoBolsas/institution/models.py
@@ -1,14 +1,16 @@
 from django.db import models
-from django.contrib.auth.models import User
+
+# Create your models here.
+
 
 class Institution(models.Model):
-    usuario = models.OneToOneField(User, on_delete=models.CASCADE)
-    nome = models.CharField(max_length=100)
-    cnpj = models.CharField(max_length=14)
-    contato = models.CharField(max_length=50)
-    endereco = models.CharField(max_length=255)
-    descricao = models.TextField()
-    foto_perfil = models.ImageField(upload_to='perfil_instituicao', default='default.jpg')
+    id = models.AutoField(primary_key=True, null=False, blank=False, unique=True)
+    nome = models.CharField(max_length=100, null=False, blank=False)
+    cnpj = models.CharField(max_length=14, null=False, blank=False, unique=True)
+    contato = models.CharField(max_length=50, null=False, blank=False)
+    endereco = models.CharField(max_length=255, null=True, blank=True, )
+    descricao = models.CharField(max_length=1000, null=False, blank=False)
+    #foto_perfil = models.ImageField(upload_to='perfil_instituicao', default='default.jpg')
 
     def __str__(self):
         return self.nome
