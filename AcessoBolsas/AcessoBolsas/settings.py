b0cf0a0e041c47d0b77af70bb5c5c8d677c59329
arrudagba <gabrielarrudahash@gmail.com>
2024-06-21 20:12:05 -0300

    base.html criado

--- AcessoBolsas/AcessoBolsas/settings.py
+++ AcessoBolsas/AcessoBolsas/settings.py
@@ -50,7 +50,7 @@ ROOT_URLCONF = 'AcessoBolsas.urls'
 TEMPLATES = [
     {
         'BACKEND': 'django.template.backends.django.DjangoTemplates',
-        'DIRS': [BASE_DIR / 'templates' / 'AcessoBolsas'], 
+        'DIRS': [os.path.join(BASE_DIR, 'templates')], 
         'APP_DIRS': True,
         'OPTIONS': {
             'context_processors': [
