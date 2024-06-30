d51c8980a89f4125b9cb195413ceabf91966637d
arrudagba <gabrielarrudahash@gmail.com>
2024-06-30 00:33:30 -0300

    botões post funcionando, inscrição em bolsas

--- AcessoBolsas/scholarship/views.py
+++ AcessoBolsas/scholarship/views.py
@@ -3,6 +3,9 @@ from scholarship.forms import ScholarshipRegisterForm, ScholarshipUpdateForm
 from django.contrib import messages
 from institution.models import Institution
 from scholarship.models import Scholarship
+from django.http import JsonResponse
+from django.http import HttpResponse
+import json
 
 # Create your views here.
 
@@ -69,6 +72,18 @@ def viewScholarship(request, slug):
     
     scholarship = get_object_or_404(Scholarship, slug=slug)
     context['scholarship'] = scholarship
+
+    if request.method == 'POST':
+        data = json.loads(request.body)
+        if 'url' in data:
+            url = data.get('url', '')
+            print(f"URL recebida: {url}")
+            return JsonResponse({'status': 'success', 'url': url})
+        elif 'action' in data and data['action'] == 'contact':
+            contato = scholarship.instituicao.contato
+            print(f"Contato da instituição: {contato}")
+            return JsonResponse({'status': 'success', 'contato': contato})
+
     return render(request, 'scholarship/viewScholarship.html', context)
 
 
@@ -103,3 +118,19 @@ def listScholarships(request):
     scholarships = Scholarship.objects.all()
     context['scholarships'] = scholarships
     return render(request,'scholarship/listScholarships.html', context)
+
+def inscrever_scholarship(request, slug):
+    scholarship = get_object_or_404(Scholarship, slug=slug)
+    if request.method == 'POST':
+        # Adicione aqui a lógica para inscrever o usuário na bolsa
+        # Por exemplo, adicionar o usuário a uma lista de inscritos na bolsa
+        return redirect('scholarship:view-scholarship', slug=scholarship.slug)
+    return HttpResponse(status=405)  # Método não permitido se não for POST
+
+def listar_scholarships_user(request):
+    user_scholarships = Scholarship.objects.filter(inscritos=request.user)
+    return render(request, 'scholarship/listScholarshipsUser.html', {'scholarships': user_scholarships})
+
+
+
+
